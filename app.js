cbb20153de073f9f0a515c242bb0443fc13775c2
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 09:30:05 -0600

    Updating app.js with some middleware calls

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ const app = express();
 
 
 
+app.use('/add-product', (req, res, next) => {
+    console.log('SECOND middlewar after next()');
+    res.send('<h1>YO</h1>')
+})
 
 
-
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('SECOND middlewar after next()');
     res.send('<h1>YO</h1>')
 })
