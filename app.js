7927f252f2d2a064685ee0114a538c95611bb940
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 17:07:38 -0600

    Updating middleware routes

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const express = require('express');
 const app = express();
 
 
-//# function passing through app.use() will be executed for every incoming request
+//* function passing through app.use() will be executed for every incoming request
 // app.use((req, res, next) => {
 //     console.log('middleware');
-//# next()- Allows the request to continue to the next middleware in line
+//* next()- Allows the request to continue to the next middleware in line
 //     next();
 // })
 
 
+
+//# urlencoded since body-parser already included
 app.use(express.urlencoded({ extended: true }));
 
 //* Will run for every request because '/'
@@ -22,17 +24,22 @@ app.use('/', (req, res, next) => {
 });
 
 
+
+
 app.use('/add-product', (req, res, next) => {
     console.log('/add-product called');
     res.send('<html><body><h1>Add Product</h1><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></body></html>')
 });
 
-app.use('/product', (req, res, next) => {
+app.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
 
-//* Had to block favicon.ico request to stop this from being called.
+
+
+
+//# Had to block favicon.ico request to stop this from being called.
 app.use('/', (req, res, next) => {
     console.log('SECOND middlewar after next()');
     res.send('<h1>YO</h1>')
