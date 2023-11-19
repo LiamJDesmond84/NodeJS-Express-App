1cafccab6b99af4d01815e490b078748263e3b2f
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 09:26:05 -0600

    Updating app.js with some next() practice

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 
 const app = express();
 
 
 //# function passing through app.use() will be executed for every incoming request
-app.use((req, res, next) => {
-    console.log('middleware');
-    next(); // Allows the request to continue to the next middleware in line
-})
+// app.use((req, res, next) => {
+//     console.log('middleware');
+//     next(); // Allows the request to continue to the next middleware in line
+// })
 
 app.use((req, res, next) => {
     console.log('SECOND middlewar after next()');
