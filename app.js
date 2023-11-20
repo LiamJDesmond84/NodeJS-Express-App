d37874539ae14b4327bc9952701a823baaf64d56
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 19:07:33 -0600

    Updating middleware routes

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/product', (req, res, next) => {
 //# Had to block favicon.ico request to stop this from being called.
 app.use('/', (req, res, next) => {
     console.log('SECOND middlewar after next()');
-    res.send('<h1>YO</h1>')
+    res.send('<h1>Hello from Express!</h1>')
 });
 
 
