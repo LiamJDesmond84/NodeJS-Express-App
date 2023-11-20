a8a96207e51405f75ed594ae3197174a9856aab0
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 19:14:14 -0600

    Moving routes to new files

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 
 //* function passing through app.use() will be executed for every incoming request
 // app.use((req, res, next) => {
@@ -17,33 +20,19 @@ const app = express();
 //# urlencoded since body-parser already included
 app.use(express.urlencoded({ extended: true }));
 
-//* Will run for every request because '/'
-app.use('/', (req, res, next) => {
-    console.log('This always runs');
-    next();
-});
+// //* Will run for every request because '/'
+// app.use('/', (req, res, next) => {
+//     console.log('This always runs');
+//     next();
+// });
 
 
 
 
-app.use('/add-product', (req, res, next) => {
-    console.log('/add-product called');
-    res.send('<html><body><h1>Add Product</h1><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></body></html>')
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
 
 
 
 
-//# Had to block favicon.ico request to stop this from being called.
-app.use('/', (req, res, next) => {
-    console.log('SECOND middlewar after next()');
-    res.send('<h1>Hello from Express!</h1>')
-});
 
 
 // const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 
-router.use('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
     console.log('/add-product called');
     res.send('<html><body><h1>Add Product</h1><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></body></html>')
 });
