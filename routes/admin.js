2bb28b5edb20f49e54dab22a301ae920123b1854
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 19:11:44 -0600

    Moving routes to new files

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
 
 
 
-app.use('/add-product', (req, res, next) => {
+router.use('/add-product', (req, res, next) => {
     console.log('/add-product called');
     res.send('<html><body><h1>Add Product</h1><form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></body></html>')
 });
 
-app.post('/product', (req, res, next) => {
+router.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
+
+
+module.exports.router = router;
+
+
--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,2 @@
 
 
-//# Had to block favicon.ico request to stop this from being called.
-app.use('/', (req, res, next) => {
-    console.log('SECOND middlewar after next()');
-    res.send('<h1>Hello from Express!</h1>')
-});
