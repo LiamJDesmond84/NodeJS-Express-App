c7deefc411c22bb4b724e570c4f1402b8e3eca9f
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-20 10:08:49 -0600

    Adding error handling

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(adminRoutes);
 
 //* 404 handler
 app.use((req, res, next) => {
-    res.sendStatus(404).send('<h1>Page not found</h1>');
+    res.status(404).send('<h1>Page not found</h1>');
 })
 
 
