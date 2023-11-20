bc7e0bb39b36555b47251d6f9160d359a017e2b5
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-19 19:12:32 -0600

    Moving routes to new files

--- routes/shop.js
+++ routes/shop.js
@@ -1,2 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
 
 
+//# Had to block favicon.ico request to stop this from being called.
+router.use('/', (req, res, next) => {
+    console.log('SECOND middlewar after next()');
+    res.send('<h1>Hello from Express!</h1>')
+});
