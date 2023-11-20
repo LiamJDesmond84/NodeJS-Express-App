9a86939b0a51bcd76c98246ba3348b35a0628d8c
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-20 10:16:55 -0600

    Updating route paths

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('/add-product called');
-    res.send('<html><body><h1>Add Product</h1><form action="/add-product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></body></html>')
+    res.send('<html><body><h1>Add Product</h1><form action="/admin/add-product" method="POST"><input type="text" name="title"/><button type="submit">Submit</button></form></body></html>')
 });
 
 router.post('/add-product', (req, res, next) => {
