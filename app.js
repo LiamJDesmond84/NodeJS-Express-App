5fbfe04a0210140fe626756374898065c31d451c
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 13:29:31 -0600

    Adding CSS

--- app.js
+++ app.js
@@ -7,23 +7,24 @@ const rootDir = require('./util/path');
 
 const app = express();
 
-// For CSS imports
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 //* function passing through app.use() will be executed for every incoming request
+
 //# urlencoded since body-parser already included
 app.use(express.urlencoded({ extended: true }));
+// For CSS imports
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 //* 404 handler - Catch-all route
-app.use('/',(req, res, next) => {
+app.use('/', (req, res, next) => {
 
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
