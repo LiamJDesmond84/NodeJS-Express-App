3cf2f3b448942589280c08824ba6e8c4b77f130d
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 10:26:50 -0600

    Cleaning up code

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(shopRoutes);
 
 //* 404 handler - Catch-all route
 app.use('/',(req, res, next) => {
-    // res.status(404).send('<h1>Page not found!!!</h1>');
+
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ const rootDir = require('../util/path');
 //# Had to block favicon.ico request to stop this from being called.
 router.get('/', (req, res, next) => {
     console.log('SHOP JS');
-    // res.send('<h1>Hello from Express!</h1>')
+
     // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
     res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 });
