68db275048fdf31880ea870239f14cc21cb2f3fa
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 11:14:21 -0600

    Adding CSS

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const rootDir = require('./util/path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
