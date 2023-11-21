c412bd606c4073452f57308912f9488ba29b0e23
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 10:21:46 -0600

    Adding util file

--- app.js
+++ app.js
@@ -27,5 +27,3 @@ app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
 
-// module.exports = path.dirname(process.mainModule.filename);
-// module.exports = path.dirname(require.main.filename);
