e5f42fc3a8174a7bb0d3ce6219c41d4bcc414be2
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 10:28:20 -0600

    Cleaning up code

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const path = require('path');
 
+const rootDir = require('./util/path');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin')
@@ -19,7 +21,7 @@ app.use(shopRoutes);
 //* 404 handler - Catch-all route
 app.use('/',(req, res, next) => {
 
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 
