2a41cf7f51db9b11a8503ca0a7edbaf75ec5accc
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-20 18:28:29 -0600

    Serving HTML files via routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+const path = require('path');
+
 const router = express.Router();
 
 
 //# Had to block favicon.ico request to stop this from being called.
 router.get('/', (req, res, next) => {
     console.log('SHOP JS');
-    res.send('<h1>Hello from Express!</h1>')
+    // res.send('<h1>Hello from Express!</h1>')
+    res.sendFile('/views/shop.html')
 });
 
 module.exports = router;
