1b758fa0da276dcc65fa8e9835fa0b58ae031bd5
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 13:35:26 -0600

    Adding dynamic data

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,32 @@
 const express = require('express');
-
 const router = express.Router();
 
 const path = require('path');
-
 const rootDir = require('../util/path');
+
+const products = [];
 
 
 // - /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    console.log('/add-product called');
     
     // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+
+
 // - /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+
+    // console.log(req.body);
+
+    products.push(req.body); 
+    
+    for(x in products) {
+        console.log(products[x]);
+    }
+
     res.redirect('/');
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,13 @@
 const express = require('express');
-
 const router = express.Router();
 
 const path = require('path');
-
 const rootDir = require('../util/path');
 
 
 
 //# Had to block favicon.ico request to stop this from being called.
 router.get('/', (req, res, next) => {
-    console.log('SHOP JS');
 
     // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
     res.sendFile(path.join(rootDir, 'views', 'shop.html'))
