fc57d70e472c75abe32e5893f79b5791a2b7a16d
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 10:15:53 -0600

    Adding 404 page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // const http = require('http');
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin')
@@ -16,7 +18,8 @@ app.use(shopRoutes);
 
 //* 404 handler - Catch-all route
 app.use('/',(req, res, next) => {
-    res.status(404).send('<h1>Page not found!!!</h1>');
+    // res.status(404).send('<h1>Page not found!!!</h1>');
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 
