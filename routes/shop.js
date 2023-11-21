368ed85a3ff4469036ccdecbc296ce3ffaf0e31c
Liam James Desmond <liamjdesmond84@gmail.com>
2023-11-21 10:25:07 -0600

    Adding util file

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
+const router = express.Router();
+
 const path = require('path');
 
-const router = express.Router();
+const rootDir = require('../util/path');
+
 
 
 //# Had to block favicon.ico request to stop this from being called.
 router.get('/', (req, res, next) => {
     console.log('SHOP JS');
     // res.send('<h1>Hello from Express!</h1>')
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+    // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 });
 
 module.exports = router;
